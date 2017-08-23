bb31971c84d7eaa546150d0c2513a1561109b7a9
Johnny Hsu <hsuyuwei@Johnnys-Macbook-Pro.local>
2017-08-23 10:43:19 -0400

    upload solutions

--- 020_valid_parentheses.py
+++ 020_valid_parentheses.py
@@ -6,30 +6,64 @@ The brackets must close in the correct order, "()" and "()[]{}" are all valid bu
 '''
 
 
+'''
+idea:
+Use the stack to solve this problem
+Scan the string from left to righ and every time we see the opening parentheses,
+for example, '(', '[', '{'
+we push it to stack because we want the last opening parentheses to be closed  first (FILO)
 
+Then when wee see the closing parentheses,
+we check whether the last opend parentheses is corresponding to the closing match,
+by poping the element from stack. If it's a valid match, then we check next one, if not, return false.
+
+At meantime, check if the stack is empty, return false.
+It means there is no opening parentheses corresponding to the closing one.
+
+In the end, after check all parentheses in the string, check if stack is empty or not.
+If not empty, retun false. It the opening parentheses can't not match any closing parentheses.
+'''
 
 class Solution:
-    def isValid(self, s):
+    def isValid_1(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        '''
+        Use Python build-in List as Stack and
+        build-in Dictionary as Hashtable (map) to lookup the open parethesis as key, closing parethesis as values
+        '''
+        #  edge case
+        if len(s) % 2 == 1:
+            return False
+
+
+        stack = []
+        # [].append() is equal to stack.push()
+        # [].pop() is equal to stack.pop()
+        # len([]) == 0 is equal to stack.empty()
+        parentheses_map = {'(': ')', '[':']', '{': '}'}
+
+        for char in s:
+            # check the char is opening parethesis
+            if char in parentheses_map:
+                stack.append(char)
+            else:
+                if len(stack) == 0 or parentheses_map[stack.pop()] != char:
+                    return False
+        return len(stack) == 0
+
+
+
+    def isValid_2(self, s):
         """
         :type s: str
         :rtype: bool
         """
         '''
-        idea:
-        Use the stack to solve this problem
-        Scan the string from left to righ and every time we see the opening parentheses,
-        for example, '(', '[', '{'
-        we push it to stack because we want the last opening parentheses to be closed  first (FILO)
-
-        Then when wee see the closing parentheses,
-        we check whether the last opend parentheses is corresponding to the closing match,
-        by poping the element from stack. If it's a valid match, then we check next one, if not, return false.
-
-        At meantime, check if the stack is empty, return false.
-        It means there is no opening parentheses corresponding to the closing one.
-
-        In the end, after check all parentheses in the string, check if stack is empty or not.
-        If not empty, retun false. It the opening parentheses can't not match any closing parentheses.
+        Create class Stack on your own.
         '''
 
 
@@ -58,12 +92,12 @@ class Solution:
             if char == parentheses_pair[0]:
                 return True
         return False
+        
 
     def match_parenthese(self, opening_p, closing_p, parentheses):
         '''
         This a a helper function to check whether the opening parethesis and closing parethesis are match.
         '''
-
         for parentheses_pair in parentheses:
             if opening_p == parentheses_pair[0] and closing_p == parentheses_pair[1]:
                 return True
